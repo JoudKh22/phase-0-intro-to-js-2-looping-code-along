47388b25fb0242650e1225cdd886ba5727ed8351
Joud Khanji <joud.hanji@gmail.com>
2023-07-20 17:40:49 +0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+function writeCards(names) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  const names = ["Guadalupe", "Ollie", "Aki"];
+  console.log(writeCards(names));
+
+  
+  function countDown(startingNumber) {
+    if (typeof startingNumber !== 'number' || startingNumber <= 0 || !Number.isInteger(startingNumber)) {
+      throw new Error('Please provide a positive integer as input.');
+    }
+  
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  countDown(10);
